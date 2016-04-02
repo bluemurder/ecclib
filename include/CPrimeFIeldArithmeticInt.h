bcc37e13aa167c5debf17683625b0a15b743759c
bluemurder <bluemurder@engineer.com>
2016-04-02 23:47:08 +0200

    Code refactor

--- include/CPrimeFIeldArithmeticInt.h
+++ include/CPrimeFIeldArithmeticInt.h
@@ -38,6 +38,31 @@
 * Warning: no null pointer deferencing check is performed on current
 * library functions. It is responsibility of the developer to provide valid
 * data to present code.
+*
+* Some references for following algorithms:
+* [1] D. Hankerson, S. Vanstone, and A.J. Menezes. Guide to Elliptic Curve
+* Cryptography. Springer Professional Computing. Springer, 2004.
+* [2] W. Hasenplaugh, G. Gaubatz, V. Gopal. Fast Modular Reduction.
+* Proceedings of ARITH'07, 2007.
+* [3] A.J. Menezes, P.C. van Oorschot, S.A. Vanstone. Handbook of Applied 
+* Cryptography. Taylor & Francis, 2010.
+* [4] D.E. Knuth, The Art of Computer Programming – Seminumerical Algorithms,
+* volume 2, Addison-Wesley, Reading, Massachusetts, 2nd edition, 1981.
+* [5] C. Koc, High-speed RSA implementation, Technical Report, RSA 
+* Laboratories, 1994.
+* [6] K. Geddes, S. Czapor, and G. Labahn, Algorithms for Computer Algebra, 
+* Kluwer Academic Publishers, Boston, 1992.
+* 
+*
+* Notes:
+* From [3] some notes on best algorithms for multiprecsion arithmetic are 
+* available. In general for modulo reduction, Montgomery reduction and barrett
+* reduction are comparable. Concerning multiplication, the Karatsuba-Ofman 
+* algorithm is considered a valid solution to improve efficiency of the 
+* classical multiplication. For a more detailed treatment of the Karatsuba-
+* Ofman algorithm, see Knuth [4], Koc [5], and Geddes, Czapor, and Labahn
+* [6]. A detailed treatment of methods for performing modular multiplication
+* can be found in Knuth [4].
 */
 
 #ifndef CPRIMEFIELFARITHMETICINT_H
@@ -60,7 +85,7 @@ struct _field_t
 	unsigned int chunksNumber;	//!< Number of chunks of every field element
 	element_t characteristics;	//!< Field characteristics
 	element_t mu;				//!< Barrett reduction precomputed term
-	unsigned int muReady;		//!< Set to 1 when mu is already computed, 0 otherwise
+	unsigned int muReady;		//!< 1 when mu is computed, 0 otherwise
 };
 
 typedef struct _field_t field_t;
@@ -69,34 +94,86 @@ unsigned int GreaterOrEqual(element_t * a, element_t * b, field_t * field);
 
 unsigned int Equals(element_t * a, element_t * b, field_t * field);
 
-void SetString(char * hexString, unsigned int chunksNumber, unsigned int bitSize, chunk_t * data);
+//! Writes the hexdecimal value from hexString to data, strictly following 
+//! the specified bitsize
+//! \param hexString The hexdecimal value to write (MSB letf)
+//! \param chunksNumber The number of chunks already allocated
+//! \param bitSize The size in bits of the underlying field
+//! \param data The vector of chunks to fill
+void SetString(
+	char * hexString, 
+	unsigned int chunksNumber,
+	unsigned int bitSize, 
+	chunk_t * data);
 
+//! Give a string containing the hexadecimal representation of the number 
+//! stored in element. This is not an efficient implementation, use only for 
+//! debug purposes.
+//! \param chunksNumber The number of chunks of given data array
+//! \param bitSize The size in bits of the number to print
+//! \param data The multiprecision number data
+//! \returns The hexadecimal representation string, remember to free such
+//! memory
 char * GetString(unsigned int chunksNumber, unsigned int bitSize, chunk_t * data);
 
+//! Sets the specified bit size and characteristics in the field object
+//! \param field The field to fill (must point to already allocated memory)
+//! \param fieldBitSize The field bis size
+//! \param characteristics Hex string describing the field characteristics
 void SetField(field_t * field, unsigned int fieldBitSize, char * characteristics);
 
+//! Sets the specified value expressed via a hex string in the field element 
+//! object. Needs the presence of a field_t object storing field bitsize, 
+//! chunks number and characteristics.
+//! \param element The target element to write
+//! \param hexString The string value to write
+//! \param field The object storing field data
 void SetElement(element_t * element, char * hexString, field_t * field);
 
+//! Erase the memory of internal members of element
 void FreeElement(element_t * element);
 
+//! Erase the memory of internal members of field
 void FreeField(field_t * field);
 
+//! Addition of a and b over a prime field. Let p the field characteristics, 
+//! the method evaluates a+b(mod p)
+//! Algorithm 2.7 [1]
 void Addition(element_t * sum, element_t * a, element_t * b, field_t * field);
 
+//! Subtraction of b from a over a prime field. Let p the field 
+//! characteristics, the method evaluates a-b(mod p)
+//! Algorithm 2.8 [1]
 void Subtraction(element_t * sub, element_t * a, element_t * b, field_t * field);
 
+//! General modulo reduction
+//! Algorithm 2.14 [1]
+//! http://cacr.uwaterloo.ca/hac/about/chap14.pdf
 void BarrettReduction(element_t * red, element_t a, field_t * field);
 
+//! Modified Barrett modulo reduction [2]
 void ModifiedBarretReduction(element_t * red, element_t a, field_t * field);
 
+//! Fast modulo reduction for p = 2^192 − 2^64 − 1 (FIPS 186-4 prime)
+//! Algorithm 2.27 [1]
 void FastReductionFIPSp192(element_t * red, element_t * a, field_t * field);
 
+//! Fast modulo reduction for p = 2^224 − 2^96 + 1 (FIPS 186-4 prime)
+//! Algorithm 2.28 [1]
 void FastReductionFIPSp224(element_t * red, element_t * a, field_t * field);
 
+//! Fast modulo reduction for p = 2^256 - 2^224 + 2^192 + 2^96 - 1 
+//! (FIPS 186-4 prime)
+//! Algorithm 2.29 [1]
 void FastReductionFIPSp256(element_t * red, element_t * a, field_t * field);
 
+//! Fast modulo reduction for p = 2^384 - 2^128 - 2^96 + 2^32 - 1 
+//! (FIPS 186-4 prime)
+//! Algorithm 2.30 [1]
 void FastReductionFIPSp384(element_t * red, element_t * a, field_t * field);
 
+//! Fast modulo reduction for p = 2^521 - 1 (FIPS 186-4 prime)
+//! Algorithm 2.31 [1]
 void FastReductionFIPSp521(element_t * red, element_t * a, field_t * field);
 
 #endif // CPRIMEFIELFARITHMETICINT_H
--- src/CPrimeFieldArithmetic.c
+++ src/CPrimeFieldArithmetic.c
@@ -17,22 +17,11 @@
 \***********************************************************************/
 
 //! \file CPrimeFieldArithmetic.c Definitions for current library methods.
-//! Some references for following algorithms:
-//! [1] D. Hankerson, S. Vanstone, and A.J. Menezes. Guide to Elliptic Curve
-//! Cryptography. Springer Professional Computing. Springer, 2004.
-//! [2] W. Hasenplaugh, G. Gaubatz, V. Gopal. Fast Modular Reduction.
-//! Proceedings of ARITH'07, 2007.
 
 #include "../include/CPrimeFieldArithmeticInt.h"
 #include <malloc.h>
 #include <string.h>
 
-//! Writes the hexdecimal value from hexString to data, strictly following 
-//! the specified bitsize
-//! \param hexString The hexdecimal value to write (MSB letf)
-//! \param chunksNumber The number of chunks already allocated
-//! \param bitSize The size in bits of the underlying field
-//! \param data The vector of chunks to fill
 void SetString(char * hexString, unsigned int chunksNumber, unsigned int bitSize, chunk_t * data)
 {
 	size_t hexStringLen = strlen(hexString);
@@ -107,14 +96,6 @@ void SetString(char * hexString, unsigned int chunksNumber, unsigned int bitSize
 	}
 }
 
-//! Give a string containing the hexadecimal representation of the number 
-//! stored in element. This is not an efficient implementation, use only for 
-//! debug purposes.
-//! \param chunksNumber The number of chunks of given data array
-//! \param bitSize The size in bits of the number to print
-//! \param data The multiprecision number data
-//! \returns The hexadecimal representation string, remember to free such
-//! memory
 char * GetString(unsigned int chunksNumber, unsigned int bitSize, chunk_t * data)
 {
 	unsigned int characters = bitSize / 4;
@@ -154,10 +135,6 @@ char * GetString(unsigned int chunksNumber, unsigned int bitSize, chunk_t * data
 	return hexdump;
 }
 
-//! Sets the specified bit size and characteristics in the field object
-//! \param field The field to fill (must point to already allocated memory)
-//! \param fieldBitSize The field bis size
-//! \param characteristics Hex string describing the field characteristics
 void SetField(field_t * field, unsigned int fieldBitSize, char * characteristics)
 {
 	// Assign field bits
@@ -177,12 +154,6 @@ void SetField(field_t * field, unsigned int fieldBitSize, char * characteristics
 
 }
 
-//! Sets the specified value expressed via a hex string in the field element 
-//! object. Needs the presence of a field_t object storing field bitsize, 
-//! chunks number and characteristics.
-//! \param element The target element to write
-//! \param hexString The string value to write
-//! \param field The object storing field data
 void SetElement(element_t * element, char * hexString, field_t * field)
 {
 	element->data = (chunk_t *)malloc(field->chunksNumber * (sizeof(chunk_t)));
@@ -190,13 +161,12 @@ void SetElement(element_t * element, char * hexString, field_t * field)
 	SetString(hexString, field->chunksNumber, field->bits, element->data);
 }
 
-//! Erase the memory of internal members of element
 void FreeElement(element_t * element)
 {
 	free(element->data);
 }
 
-//! Erase the memory of internal members of field
+
 void FreeField(field_t * field)
 {
 	FreeElement(&field->characteristics);
@@ -244,9 +214,6 @@ unsigned int Equals(element_t * a, element_t * b, field_t * field)
 	return 1;
 }
 
-//! Addition of a and b over a prime field. Let p the field characteristics, 
-//! the method evaluates a+b(mod p)
-//! Algorithm 2.7 [1]
 void Addition(element_t * sum, element_t * a, element_t * b, field_t * field)
 {
 	// Binary sum
@@ -289,9 +256,6 @@ void Addition(element_t * sum, element_t * a, element_t * b, field_t * field)
 	}
 }
 
-//! Subtraction of b from a over a prime field. Let p the field 
-//! characteristics, the method evaluates a-b(mod p)
-//! Algorithm 2.8 [1]
 void Subtraction(element_t * sub, element_t * a, element_t * b, field_t * field)
 {
 	// Binary subtraction
@@ -333,9 +297,6 @@ void Subtraction(element_t * sub, element_t * a, element_t * b, field_t * field)
 	}
 }
 
-//! General modulo reduction
-//! Algorithm 2.14 [1]
-//! http://cacr.uwaterloo.ca/hac/about/chap14.pdf
 void BarrettReduction(element_t * red, element_t a, field_t * field)
 {
 	if (!field->muReady)
@@ -351,14 +312,11 @@ void BarrettReduction(element_t * red, element_t a, field_t * field)
 	}
 }
 
-//! Modified Barrett modulo reduction [2]
 void ModifiedBarretReduction(element_t * red, element_t a, field_t * field)
 {
 
 }
 
-//! Fast modulo reduction for p = 2^192 − 2^64 − 1 (FIPS 186-4 prime)
-//! Algorithm 2.27 [1]
 void FastReductionFIPSp192(element_t * red, element_t * a, field_t * field)
 {
 #if ARCHITECTURE_BITS == 8
@@ -732,8 +690,6 @@ void FastReductionFIPSp192(element_t * red, element_t * a, field_t * field)
 #endif
 }
 
-//! Fast modulo reduction for p = 2^224 − 2^96 + 1 (FIPS 186-4 prime)
-//! Algorithm 2.28 [1]
 void FastReductionFIPSp224(element_t * red, element_t * a, field_t * field)
 {
 #if ARCHITECTURE_BITS == 8
@@ -1217,8 +1173,6 @@ void FastReductionFIPSp224(element_t * red, element_t * a, field_t * field)
 #endif
 }
 
-//! Fast modulo reduction for p = 2^256 - 2^224 + 2^192 + 2^96 - 1 (FIPS 186-4 prime)
-//! Algorithm 2.29 [1]
 void FastReductionFIPSp256(element_t * red, element_t * a, field_t * field)
 {
 #if ARCHITECTURE_BITS == 8
@@ -2080,8 +2034,6 @@ void FastReductionFIPSp256(element_t * red, element_t * a, field_t * field)
 #endif
 }
 
-//! Fast modulo reduction for p = 2^384 - 2^128 - 2^96 + 2^32 - 1 (FIPS 186-4 prime)
-//! Algorithm 2.30 [1]
 void FastReductionFIPSp384(element_t * red, element_t * a, field_t * field)
 {
 #if ARCHITECTURE_BITS == 8
@@ -3323,8 +3275,6 @@ void FastReductionFIPSp384(element_t * red, element_t * a, field_t * field)
 #endif
 }
 
-//! Fast modulo reduction for p = 2^521 - 1 (FIPS 186-4 prime)
-//! Algorithm 2.31 [1]
 void FastReductionFIPSp521(element_t * red, element_t * a, field_t * field)
 {
 #if ARCHITECTURE_BITS == 8
